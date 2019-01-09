6fa3576660e043c07a0e58c39cffb5baeb206727
Alexis Prevost-Maynen <killer76520@gmail.com>
2019-01-09 09:54:29 +0100

    End pages post - prismic

--- app/views/post.php
+++ app/views/post.php
@@ -56,16 +56,10 @@ $uid = $WPGLOBAL['uid'];
 							switch ($slice->slice_type) {
 
 								case 'citation':
-									RichText::asText($slice->primary->quote);
-									var_dump($author);
 									echo '<div class="container-quote">
 											<img class="obj" src="/img/post/icn-quote.svg" alt="">
-											<p>'.
-												RichText::asText($slice->primary->quote)
-											.'</p>
-											<div class="name">'.
-												RichText::asText($slice->primary->author)
-											.'</div>
+											<p>'.$slice->primary->quote.'</p>
+											<div class="name">'.$slice->primary->author.'</div>
 										  </div>';
 									break;
 
@@ -74,42 +68,17 @@ $uid = $WPGLOBAL['uid'];
 									break;
 
 								case 'image':
+									echo '<img src="'.$slice->primary->image->url.'" alt="">';
 									break;
 
 								case 'youtube':
+									echo '<iframe src="'.$slice->primary->youtube_video_id->url.'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
 									break;
 								
 							}
 						}
 
 						?>
-						
-						<!--
-						<div class="container-quote">
-							<img class="obj" src="/img/post/icn-quote.svg" alt="">
-							<p>
-								<?= RichText::asText($document->content_citation_text); ?>
-							</p>
-							<div class="name">
-								<?= RichText::asText($document->content_citation_name); ?>
-							</div>
-						</div>
-						<p>
-							<?= RichText::asText($document->content_text_1); ?>
-							<img src="<?= $document->content_image_1->url ?>" alt="">
-						</p>
-						
-						<h2>
-							<?= RichText::asText($document->content_title_2); ?>
-						</h2>
-						<?= RichText::asHtml($document->content_text_2); ?>
-						<h3>
-							<?= RichText::asText($document->content_title_3); ?>
-						</h3>
-						<p>
-							<?= RichText::asText($document->content_text_3); ?>
-						</p>
-						<iframe src="<?= $document->content_video->url ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 						<div class="container-rs">
 							<a href="https://www.facebook.com/sharer/sharer.php?u=https://skeelz.com/newz/<?php echo($uid); ?>" target="_blank">
 								<img src="/img/newz/Facebook-color.svg" alt="">
@@ -120,8 +89,7 @@ $uid = $WPGLOBAL['uid'];
 							<a href="https://twitter.com/intent/tweet?url=https://skeelz.com/newz/<?php echo($uid); ?>" target="_blank">
 								<img src="/img/newz/Twitter-color.svg" alt="">
 							</a>
-
-						</div>-->
+						</div>
 					</div>
 				</div>
 			</section>
